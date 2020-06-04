26810fc859afc6310025a3433cdd163b054da166
cabosh <callum.bosher@gmail.com>
2020-06-04 22:33:15 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #write your code below this line
 
+    while True:
+
+        number = int(input("Enter a number: "))
+
+        if number < 0:
+            print("Unsuitable number")
+        elif number == 0:
+            break
+        elif number > 0:
+            print(str(number ** 2))
+
 if __name__ == '__main__':
     main()
